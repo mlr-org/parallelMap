646402a302892f61cb8b4b17669fdcc6ce1664b4
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-26 18:09:04 +0200

    show BatchJobs resources in parallelShowOptions

--- parallelMap/R/parallelShowOptions.R
+++ parallelMap/R/parallelShowOptions.R
@@ -11,12 +11,18 @@ parallelShowOptions = function() {
   mycat = function(opt) {
     opt1val = getPMOption(opt)
     opt2val = getPMDefOption(opt)
+    if (opt == "bj.resources") {
+      opt1val = ifelse(length(opt1val) == 0L, "(defaults from BatchJobs config)", 
+        listToShortString(opt1val))    
+      if (!is.null(opt2val))
+        opt2val = listToShortString(opt2val)
+    }
     if (is.null(opt2val))
       opt2val = "not set"
-    if (opt != "storagedir")
+    if (opt %nin% c("bj.resources", "storagedir"))
       catf("%-20s: %-10s (%s)", opt, opt1val, opt2val)
     else
-      catf("%-20s: %-10s\n                     (%s)", opt, opt1val, opt2val)
+      catf("%-20s: %-10s\n                      (%s)", opt, opt1val, opt2val)
   }
   catf("%-20s: %-10s (%s)", "parallelMap options", "value", "default")
   catf("")
@@ -27,4 +33,6 @@ parallelShowOptions = function() {
   mycat("logging")
   mycat("show.info")
   mycat("storagedir")
+  if (isModeBatchJobs() || identical(getPMDefOptMode(), MODE_BATCHJOBS))
+    mycat("bj.resources")
 }
