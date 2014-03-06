d6ee7d1c01f7bfcf6793951f0ec7a8b1360ab30e
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-06 19:13:30 +0100

    use new testthat structure

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,7 +1,5 @@
- library(testthat)
- library(BBmisc)
-
-test_package("parallelMap")
+library(testthat)
+test_check("parallelMap")
 
 #FIXME: bad hack 
 # for some reason using test_package and opening a socked node
@@ -9,7 +7,6 @@ test_package("parallelMap")
 # but we really want to test at least one real parallel mode on cran 
  
 if (!interactive()) {
-  library(parallelMap)
   source(system.file("tests/helpers.R", package="parallelMap"))
   source(system.file("tests/helper_sockettest.R", package="parallelMap"))
   sockettest() 
