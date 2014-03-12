e8551469b1f14c7349dab6cd7af1b171cfa3158f
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-12 14:11:43 +0100

    remove BBmisc calls from onload

--- R/zzz.R
+++ R/zzz.R
@@ -7,21 +7,22 @@ MODE_SOCKET = "socket"
 MODE_MPI = "mpi"
 MODE_BATCHJOBS = "BatchJobs"
 MODES = c(MODE_LOCAL, MODE_MULTICORE, MODE_SOCKET, MODE_MPI, MODE_BATCHJOBS)
-  
+
 STATUS_STARTED = "started"
 STATUS_STOPPED = "stopped"
 
 .onLoad = function(libname, pkgname) {
   # init all settings from defaults
+  # we cant call any function here in onload that dispatch to BBmisc...
   options(
-    parallelMap.mode = getPMDefOptMode(),
-    parallelMap.cpus = getPMDefOptCpus(),
-    parallelMap.socket.hosts = getPMDefOptSocketHosts(),
-    parallelMap.level = getPMDefOptLevel(),
-    parallelMap.logging = getPMDefOptLogging(),
-    parallelMap.autostart = getPMDefOptAutostart(),
-    parallelMap.show.info = getPMDefOptShowInfo(),
-    parallelMap.storagedir = getPMDefOptStorageDir(),
+    parallelMap.mode = getPMDefOption("mode", MODE_LOCAL),
+    parallelMap.cpus = getPMDefOption("cpus", NA_integer_),
+    parallelMap.socket.hosts = getPMDefOption("socket.hosts", NULL),
+    parallelMap.level = getPMDefOption("level", NA_character_),
+    parallelMap.logging = getPMDefOption("logging", FALSE),
+    parallelMap.autostart = getPMDefOption("autostart", TRUE),
+    parallelMap.show.info = getPMDefOption("show.info", TRUE),
+    parallelMap.storagedir = getPMDefOption("storagedir", getwd()),
     parallelMap.status = STATUS_STOPPED,
     parallelMap.on.slave = FALSE,
     parallelMap.registered.levels = list()
