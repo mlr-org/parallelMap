7156f3eab25a6480fa59da9e8091f563c5c215dd
Bernd Bischl <bernd_bischl@gmx.net>
2015-01-27 04:12:30 +0100

    import parallel and clean up NOTES

--- R/getExtraPackages.R
+++ R/getExtraPackages.R
@@ -1,7 +1,5 @@
 getExtraPackages = function(mode) {
-  if (mode %in% c(MODE_MULTICORE, MODE_SOCKET))
-    "parallel"
-  else if (mode %in% MODE_MPI)
+  if (mode %in% MODE_MPI)
     c("Rmpi", "parallel")
   else if (mode %in% MODE_BATCHJOBS)
     "BatchJobs"
