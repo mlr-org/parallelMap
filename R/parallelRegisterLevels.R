96e1d9f663774598b32b16c14c5e0e6bac38cb20
Bernd Bischl <bernd_bischl@gmx.net>
2015-02-02 19:08:41 +0100

    ShowRegisteredLevs --> getRegistredLevs

--- R/parallelRegisterLevels.R
+++ R/parallelRegisterLevels.R
@@ -8,26 +8,27 @@
 #'
 #' To avoid name clases, we encourage developes to always sepecify the argument \code{package}.
 #' This will prefix the specified levels with the string containing the package name, e.g.
-#' \code{parallelRegisterLevels("parallelMap", "dummy")} will register the level \dQuote{parallelMap.dummy}
-#' and users can start parallelization for this level with
+#' \code{parallelRegisterLevels(package="foo", levels="dummy")}
+#' will register the level \dQuote{foo.dummy} and users can start parallelization for this level with
 #' \code{parallelStart(<backend>, level = "parallelMap.dummy")}.
-#' If you do not provide \code{package}, the level names will be taken as-is and sorted in will be put
-#' into the category \dQuote{interactive} by \code{\link{parallelGetRegisteredLevels}}.
+#' If you do not provide \code{package}, the level names will be associated with category
+#' \dQuote{custom} and can there be later refered to with \dQuote{custom.dummy}.
 #'
 #' @param package [\code{character(1)}]\cr
-#'   Name of your package. Default is \code{NA} (no package, interactive use).
+#'   Name of your package.
+#'   Default is \dQuote{custom} (we are not in a package).
 #' @param levels [\code{character(1)}]\cr
 #'   Availabe levels that are used in the \code{\link{parallelMap}} operations of your package
 #'   or code.
 #'   If \code{package} is not missing, all levels will be prefixed with \dQuote{[package].}.
 #' @return Nothing.
 #' @export
-parallelRegisterLevels = function(package = NA_character_, levels) {
-  assertString(package, na.ok = TRUE)
-  assertCharacter(levels, any.missing = FALSE)
+parallelRegisterLevels = function(package = "custom", levels) {
+  assertString(package)
+  assertCharacter(levels, min.len = 1L, any.missing = FALSE)
   reg.levs = getPMOption("registered.levels", list())
   if (is.na(package)) {
-    reg.levs[["interactive"]] = union(reg.levs[["interactive"]], levels)
+    reg.levs[["custom"]] = union(reg.levs[["custom"]], levels)
   } else {
     reg.levs[[package]] = union(reg.levs[[package]], sprintf("%s.%s", package, levels))
   }
@@ -35,11 +36,27 @@ parallelRegisterLevels = function(package = NA_character_, levels) {
   invisible(NULL)
 }
 
-#' Get registered parallelization levels for all currently loaded packages.
+#' @title Get registered parallelization levels for all currently loaded packages.
 #'
-#' @return Returns a named list with all registered levels.
+#' @description
+#' The S3 object only has one slot, which is called \code{levels}.
+#' This contains a named list. Each name refers to \code{package} from the call to
+#' \code{\link{parallelRegisterLevels}}, while the entries are character
+#' vectors of the form \dQuote{package.level}.
+#' @return [code{RegisteredLevels}]. See above.
 #' @export
 parallelGetRegisteredLevels = function() {
-  reg.levs = getPMOption("registered.levels", list())
-  reg.levs[order(names(reg.levs))]
+  lvls = getPMOption("registered.levels", list())
+  makeS3Obj("RegisteredLevels", levels = lvls)
 }
+
+#' @export
+print.RegisteredLevels = function(x, ...) {
+  levs = parallelGetRegisteredLevels()$levels
+  ns = names(levs)
+  for (i in seq_along(levs)) {
+    catf("%s: %s", ns[i], collapse(levs[[i]], sep = ", "))
+  }
+  invisible(NULL)
+}
+
--- tests/testthat/test_registerLevels.R
+++ tests/testthat/test_registerLevels.R
@@ -1,16 +1,17 @@
 context("register levels")
 
 test_that("register levels", {
+  expect_equal(parallelGetRegisteredLevels()$levels, list())
   parallelRegisterLevels("p1", "lev1")
   parallelRegisterLevels("p2", c("a", "b"))
   parallelRegisterLevels("p2", c("a", "b"))
   expect_equal(
-    parallelGetRegisteredLevels(),
+    parallelGetRegisteredLevels()$levels,
     list(p1 = "p1.lev1", p2 = c("p2.a", "p2.b"))
   )
   parallelRegisterLevels(levels = c("x", "y"))
   expect_equal(
-    parallelGetRegisteredLevels(),
-    list("interactive" = c("x", "y"), p1 = "p1.lev1", p2 = c("p2.a", "p2.b"))
+    parallelGetRegisteredLevels()$levels,
+    list(p1 = "p1.lev1", p2 = c("p2.a", "p2.b"), custom = c("custom.x", "custom.y"))
   )
 })
