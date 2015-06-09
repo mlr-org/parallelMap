c9363c8ed0de8b7ce14406234f2d2424390ed602
Bernd Bischl <bernd_bischl@gmx.net>
2015-06-09 20:08:20 +0200

    clean up test

--- tests/testthat/test_parallelStart.R
+++ tests/testthat/test_parallelStart.R
@@ -6,6 +6,8 @@ test_that("parallelStart finds regged level", {
   parallelRegisterLevels(levels = "bla")
   parallelStartSocket(level = "custom.bla", cpus = 2L)
   parallelStop()
+  # remove regged level again for clean state
+  options(parallelMap.registered.levels = list())
   options(warn = oldopt)
 })
 
