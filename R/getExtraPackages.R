06dc13d0ab095dc5729972e8ad0ce9c272fee335
Michel Lang <michellang@gmail.com>
2016-11-08 15:38:44 +0100

    improved getExtraPackages

--- R/getExtraPackages.R
+++ R/getExtraPackages.R
@@ -1,10 +1,8 @@
 getExtraPackages = function(mode) {
-  if (mode %in% MODE_MPI)
-    "Rmpi"
-  else if (mode %in% MODE_BATCHJOBS)
-    "BatchJobs"
-  else if (mode %in% MODE_BATCHTOOLS)
-    "batchtools"
-  else
+  switch(mode,
+    MODE_MPI = "Rmpi",
+    MODE_BATCHJOBS = "BatchJobs",
+    MODE_BATCHTOOLS = "batchtools",
     character(0L)
+  )
 }
