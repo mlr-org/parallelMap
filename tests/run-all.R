a28ff25dcff78c72c5461fb4f7adf380c12df6f8
Bernd Bischl <bernd_bischl@gmx.net>
2016-11-08 15:54:10 +0100

    minor clean ups

--- tests/run-all.R
+++ tests/run-all.R
@@ -4,15 +4,15 @@
 library(testthat)
 test_check("parallelMap")
 
-#FIXME: bad hack
-# for some reason using test_package and opening a socked node
-# blocks R CMD check
-# but we really want to test at least one real parallel mode on cran
+# #FIXME: bad hack
+# # for some reason using test_package and opening a socked node
+# # blocks R CMD check
+# # but we really want to test at least one real parallel mode on cran
 
-if (!interactive()) {
+# if (!interactive()) {
   library(BBmisc)
   library(parallelMap)
   source("testthat/helpers.R")
   source("testthat/helper_sockettest.R")
   sockettest()
-}
+# }
