6a0ff25fd81b853fd7067343f53e116d739246c6
mb706 <ma.binder@campus.lmu.de>
2017-07-24 21:45:52 -0400

    Print the actual ParallelMapOptions object

--- R/parallelShowOptions.R
+++ R/parallelShowOptions.R
@@ -22,8 +22,8 @@ parallelGetOptions = function() {
 #' @export
 print.ParallelMapOptions = function(x, ...) {
   mycat = function(opt) {
-    opt1val = opts$settings[[opt]]
-    opt2val = opts$defaults[[opt]]
+    opt1val = x$settings[[opt]]
+    opt2val = x$defaults[[opt]]
     if (opt == "bj.resources") {
       opt1val = ifelse(length(opt1val) == 0L, "(defaults from BatchJobs config)",
         convertToShortString(opt1val))
@@ -37,7 +37,6 @@ print.ParallelMapOptions = function(x, ...) {
     else
       catf("%-20s: %-10s\n                      (%s)", opt, opt1val, opt2val)
   }
-  opts = parallelGetOptions()
   catf("%-20s: %-10s (%s)", "parallelMap options", "value", "default")
   catf("")
   mycat("mode")
