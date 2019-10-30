de18c1acf6330c4571379530e71d97c484c8f0ee
pat-s <patrick.schratz@gmail.com>
2019-10-30 21:42:03 +0100

    skip multicore on appveyor

--- tests/testthat/test_multicore.R
+++ tests/testthat/test_multicore.R
@@ -5,38 +5,37 @@ context("multicore mode")
 # apparently not when I really use the pkg...?
 # .Warning in selectChildren(ac, 1) :
 #  error 'Interrupted system call' in select
-if (isExpensiveExampleOk()) {
-  test_that("multicore mode", {
-    parallelStartMulticore(2)
-    partest1()
-    parallelStop()
+test_that("multicore mode", {
+  skip_on_appveyor()
+  parallelStartMulticore(2)
+  partest1()
+  parallelStop()
 
-    parallelStartMulticore(2, load.balancing = TRUE)
-    partest1()
-    parallelStop()
+  parallelStartMulticore(2, load.balancing = TRUE)
+  partest1()
+  parallelStop()
 
-    expect_error(parallelStartMulticore(storagedir = "xxx"))
+  expect_error(parallelStartMulticore(storagedir = "xxx"))
 
-    parallelStartMulticore(2, logging = TRUE, storagedir = tempdir())
-    # FIXME: see issue #33
-    partest2(tempdir(), test.warning = FALSE)
-    parallelStop()
+  parallelStartMulticore(2, logging = TRUE, storagedir = tempdir())
+  # FIXME: see issue #33
+  partest2(tempdir(), test.warning = FALSE)
+  parallelStop()
 
-    parallelStartMulticore(2, mc.preschedule = FALSE)
-    partest4(slave.error.test = FALSE)
-    parallelStop()
+  parallelStartMulticore(2, mc.preschedule = FALSE)
+  partest4(slave.error.test = FALSE)
+  parallelStop()
 
-    parallelStartMulticore(2)
-    partest5()
-    parallelStop()
+  parallelStartMulticore(2)
+  partest5()
+  parallelStop()
 
-    parallelStartMulticore(2)
-    partest6(slave.error.test = FALSE)
-    parallelStop()
+  parallelStartMulticore(2)
+  partest6(slave.error.test = FALSE)
+  parallelStop()
 
-    parallelStartMulticore(2, load.balancing = TRUE)
-    partest7()
-    parallelStop()
+  parallelStartMulticore(2, load.balancing = TRUE)
+  partest7()
+  parallelStop()
 
-  })
-}
+})
