b075fb751cb25ae953075eff300ae58f8012d6b0
pat-s <patrick.schratz@gmail.com>
2019-12-19 17:41:02 +0100

    fix

---
+++ tests/testthat/test-parallelLibrary.R
@@ -0,0 +1,14 @@
+context("parallelLibrary")
+
+# issue https://github.com/mlr-org/parallelMap/issues/67
+test_that("exported packages honor the given parallelization level", {
+
+  parallelRegisterLevels(levels = "test")
+  parallelStartSocket(1, level = "custom.test")
+  parallelLibrary("rpart")
+
+  out = parallelMap(function(.) search(), 1, level = "custom.test")[[1]]
+  parallelStop()
+
+  expect_true(any(grepl("package:rpart", out)))
+})
