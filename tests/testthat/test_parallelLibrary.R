adb1f15c492f62e54f53da7b765c42211cc08772
pat-s <patrick.schratz@gmail.com>
2020-03-24 11:12:48 +0100

    require rpart in tests

--- tests/testthat/test_parallelLibrary.R
+++ tests/testthat/test_parallelLibrary.R
@@ -2,11 +2,6 @@ context("parallelLibrary")
 
 # issue https://github.com/mlr-org/parallelMap/issues/67
 test_that("exported packages honor the given parallelization level", {
-
-  # for some reasons travis is missing package 'rpart' when loading even
-  # though its available
-  skip_on_ci()
-
   parallelRegisterLevels(levels = "test")
   parallelStartSocket(1, level = "custom.test")
   parallelLibrary(packages = "rpart")
